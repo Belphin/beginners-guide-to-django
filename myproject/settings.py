440401f31e8d930209647151a899dc068fa98699
Belphin <findolph0003@gmail.com>
2023-05-19 17:46:29 +0300

    End part 6 - Class-Based Views

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     
-		'widget_tweaks',
+    'widget_tweaks',
 
     'accounts',
     'boards',
--- myproject/urls.py
+++ myproject/urls.py
@@ -35,5 +35,6 @@ urlpatterns = [
 	path('settings/password/', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
 		name='password_change'),
 	path('settings/password/done', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
-		name='password_change_done')
+		name='password_change_done'),
+	path('settings/account/', accounts_views.UserUpdateView.as_view(), name='my_account')
 ]
